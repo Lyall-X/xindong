3b0bdb51380cb009058ba996bc15c2c71eb1ae09
1231312123 <1312321321>
2019-12-15 03:29:54 +0800

    333333

---
+++ RoguelikeProject/Assets/Scripts/Story/Act2Panel.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+/// <summary>
+/// Create time
+/// Last revision date 
+/// </summary>
+public class Act2Panel : MonoBehaviour
+{
+    public Text[] texts;
+    public string[] strings;
+    public float[] times;
+    public Font font;
+
+    public bool nestSonSpeak = false;
+
+    private int currentCout = 0;
+    private Tween currentTween = null;
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        foreach (Text item in texts)
+        {
+            item.color = Color.white;
+            item.font = font;
+            item.fontSize = 30;
+        }
+
+        //DoTween初始化
+        currentTween = texts[0].DOText("", 0);
+        DOTween.defaultAutoPlay = AutoPlay.None;
+        currentTween.SetAutoKill(false);
+
+        //获得gameManager
+        gameManager = GameManager.Instance;
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !currentTween.IsPlaying())
+        {
+            currentCout++;
+            if (nestSonSpeak)
+            {
+                texts[0].transform.parent.gameObject.SetActive(true);
+                texts[1].transform.parent.gameObject.SetActive(false);
+            }
+            else
+            {
+                texts[0].transform.parent.gameObject.SetActive(false);
+                texts[1].transform.parent.gameObject.SetActive(true);
+            }
+            if (currentCout <= strings.Length)
+            {
+                switch (currentCout)
+                {
+                    case 1:
+                    case 2:
+                    case 5:
+                    case 9:
+                        OnSession(texts[0], strings[currentCout - 1], times[currentCout - 1]);
+                        
+                        break;
+                    case 3:
+                    case 4:
+                    case 6:
+                    case 7:
+                    case 8:
+                        OnSession(texts[1], strings[currentCout - 1], times[currentCout - 1]);
+                        break;
+                }
+                if (currentCout + 1 == 2 || currentCout + 1 == 5 || currentCout + 1 == 9)
+                {
+                    nestSonSpeak = true;
+                }
+                else
+                {
+                    nestSonSpeak = false;
+                }
+            }
+            else
+            {
+                gameManager.InitGame();
+                AudioManager.Instance.PlayBgMusic(GameManager.Instance.LevelClip);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 对话
+    /// </summary>
+    /// <param name="text"></param>
+    /// <param name="Strings"></param>
+    /// <param name="time"></param>
+    private void OnSession(Text text, string Strings, float time)
+    {
+        text.transform.parent.gameObject.SetActive(true);
+        text.text = "";
+        currentTween = text.DOText(Strings, time);
+        currentTween.Play();
+    }
+
+}
---
+++ RoguelikeProject/Assets/Scripts/Story/Act4Panel.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+/// <summary>
+/// Create time
+/// Last revision date 
+/// </summary>
+public class Act4Panel : MonoBehaviour
+{
+    public Text text;
+    public string[] strings;
+    public float time;
+    public Font font;
+
+    private int currentCout = 0;
+    private Tween currentTween = null;
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        text.color = Color.white;
+        text.font = font;
+        text.fontSize = 30;
+
+        //DoTween初始化
+        currentTween = text.DOText("", 0);
+        DOTween.defaultAutoPlay = AutoPlay.None;
+        currentTween.SetAutoKill(false);
+
+        //获得gameManager
+        gameManager = GameManager.Instance;
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !currentTween.IsPlaying())
+        {
+            currentCout++;
+            if (currentCout <= strings.Length)
+            {
+                OnSession(text, strings[currentCout - 1], time);
+            }
+            else
+            {
+                gameManager.InitGame();
+                AudioManager.Instance.PlayBgMusic(GameManager.Instance.LevelClip);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 对话
+    /// </summary>
+    /// <param name="text"></param>
+    /// <param name="Strings"></param>
+    /// <param name="time"></param>
+    private void OnSession(Text text, string Strings, float time)
+    {
+        text.transform.parent.gameObject.SetActive(true);
+        text.text = "";
+        currentTween = text.DOText(Strings, time).OnComplete(() => SessionComplete(text));
+        currentTween.Play();
+    }
+
+    /// <summary>
+    /// 对话结束 OnComplete
+    /// </summary>
+    /// <param name="text"></param>
+    private void SessionComplete(Text text)
+    {
+    }
+}
