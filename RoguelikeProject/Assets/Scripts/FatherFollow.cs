ba04c1c3886977e97048e72014ac1a018bf34111
1231312123 <1312321321>
2019-12-14 11:18:44 +0800

    213123123

---
+++ RoguelikeProject/Assets/Scripts/FatherFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FatherFollow : MonoBehaviour
+{
+    public float heigh = 100;
+    public int finshLevel = 6;
+    private Image image;
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        Vector3 imagePosition = image.rectTransform.position;
+        image.rectTransform.position = new Vector3(imagePosition.x, imagePosition.y - heigh/finshLevel*GameManager.Instance.level, imagePosition.z);
+    }
+}
